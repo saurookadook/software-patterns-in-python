503d34997b215a5d936887f2dd2297cec3b5b5ac
Andy Maskiell <maskiella@gmail.com>
2025-11-09 10:55:09 -0500

    feat: implement tests for BuilderApp and CarBuilder

---
+++ software_patterns/design_patterns/creational/builders/__tests__/test_builder_app.py
@@ -0,0 +1,171 @@
+import pytest
+
+from software_patterns.design_patterns.creational.builders.constants import EngineType
+from software_patterns.design_patterns.creational.builders.entities import (
+    Car,
+    CarConfig,
+    Manual,
+)
+from software_patterns.design_patterns.creational.builders import BuilderApp
+
+test_cases = [
+    dict(
+        test_name="handles car configs for diesel cars",
+        car_configs=[
+            CarConfig(
+                engine=EngineType.DIESEL,
+                install_GPS=False,
+                install_trip_computer=True,
+                seats=5,
+            ),
+            CarConfig(
+                engine=EngineType.DIESEL,
+                install_GPS=True,
+                install_trip_computer=True,
+                seats=8,
+            ),
+            CarConfig(
+                engine=EngineType.DIESEL,
+                install_GPS=False,
+                install_trip_computer=False,
+                seats=2,
+            ),
+            CarConfig(
+                engine=EngineType.DIESEL,
+                install_GPS=True,
+                install_trip_computer=False,
+                seats=5,
+            ),
+            CarConfig(
+                engine=EngineType.DIESEL,
+                install_GPS=True,
+                install_trip_computer=False,
+                seats=7,
+            ),
+        ],
+    ),
+    dict(
+        test_name="handles car configs for electric cars",
+        car_configs=[
+            CarConfig(
+                engine=EngineType.ELECTRIC,
+                install_GPS=False,
+                install_trip_computer=True,
+                seats=5,
+            ),
+            CarConfig(
+                engine=EngineType.ELECTRIC,
+                install_GPS=True,
+                install_trip_computer=True,
+                seats=8,
+            ),
+            CarConfig(
+                engine=EngineType.ELECTRIC,
+                install_GPS=False,
+                install_trip_computer=True,
+                seats=2,
+            ),
+            CarConfig(
+                engine=EngineType.ELECTRIC,
+                install_GPS=True,
+                install_trip_computer=True,
+                seats=4,
+            ),
+        ],
+    ),
+    dict(
+        test_name="handles car configs for gasoline cars",
+        car_configs=[
+            CarConfig(
+                engine=EngineType.GASOLINE,
+                install_GPS=False,
+                install_trip_computer=False,
+                seats=2,
+            ),
+            CarConfig(
+                engine=EngineType.GASOLINE,
+                install_GPS=True,
+                install_trip_computer=False,
+                seats=7,
+            ),
+            CarConfig(
+                engine=EngineType.GASOLINE,
+                install_GPS=True,
+                install_trip_computer=True,
+                seats=5,
+            ),
+            CarConfig(
+                engine=EngineType.GASOLINE,
+                install_GPS=False,
+                install_trip_computer=True,
+                seats=4,
+            ),
+        ],
+    ),
+    dict(
+        test_name="handles car configs with mix of valid settings",
+        car_configs=[
+            CarConfig(
+                engine=EngineType.ELECTRIC,
+                install_GPS=False,
+                install_trip_computer=True,
+                seats=5,
+            ),
+            CarConfig(
+                engine=EngineType.ELECTRIC,
+                install_GPS=True,
+                install_trip_computer=True,
+                seats=8,
+            ),
+            CarConfig(
+                engine=EngineType.GASOLINE,
+                install_GPS=False,
+                install_trip_computer=False,
+                seats=2,
+            ),
+            CarConfig(
+                engine=EngineType.DIESEL,
+                install_GPS=True,
+                install_trip_computer=False,
+                seats=5,
+            ),
+            CarConfig(
+                engine=EngineType.GASOLINE,
+                install_GPS=True,
+                install_trip_computer=False,
+                seats=7,
+            ),
+        ],
+    ),
+]
+
+
+@pytest.fixture
+def builder_app():
+    return BuilderApp()
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [tuple(tc) for tc in test_cases],
+)
+def test_builder_app(builder_app, test_case):
+    _, configs = test_case
+    builder_app.make_car(configs)
+
+    result_products = builder_app.get_result_products()
+
+    assert len(result_products) == len(configs)
+
+    for i, product in enumerate(result_products):
+        config = configs[i]
+        car, manual = product
+
+        assert isinstance(car, Car)
+        assert isinstance(manual, Manual)
+
+        for prod in (car, manual):
+            assert prod.engine == config.engine
+            assert prod.GPS == config.install_GPS
+            assert prod.trip_computer == config.install_trip_computer
+            assert prod.seats == config.seats
---
+++ software_patterns/design_patterns/creational/builders/__tests__/test_car_builder.py
@@ -0,0 +1,57 @@
+import pytest
+
+from ..car_builder import CarBuilder
+from ..constants import EngineType
+
+
+@pytest.fixture
+def car_builder():
+    return CarBuilder()
+
+
+def test_car_builder_get_and_set_engine(car_builder):
+    builder_car = car_builder._car
+
+    assert builder_car.engine == None
+    assert car_builder.get_engine() == None
+
+    car_builder.set_engine(EngineType.ELECTRIC)
+
+    assert builder_car.engine == EngineType.ELECTRIC
+    assert car_builder.get_engine() == EngineType.ELECTRIC
+
+
+def test_car_builder_get_and_set_GPS(car_builder):
+    builder_car = car_builder._car
+
+    assert builder_car.GPS == False
+    assert car_builder.get_GPS() == False
+
+    car_builder.set_GPS(True)
+
+    assert builder_car.GPS == True
+    assert car_builder.get_GPS() == True
+
+
+def test_car_builder_get_and_set_seats(car_builder):
+    builder_car = car_builder._car
+
+    assert builder_car.seats == 0
+    assert car_builder.get_seats() == 0
+
+    car_builder.set_seats(7)
+
+    assert builder_car.seats == 7
+    assert car_builder.get_seats() == 7
+
+
+def test_car_builder_get_and_set_trip_computer(car_builder):
+    builder_car = car_builder._car
+
+    assert builder_car.trip_computer == False
+    assert car_builder.get_trip_computer() == False
+
+    car_builder.set_trip_computer(True)
+
+    assert builder_car.trip_computer == True
+    assert car_builder.get_trip_computer() == True
